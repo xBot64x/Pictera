a59aa9308a8c51b87cf47515c9a0abafccb1f4c2
xBot64x <filipknize@gmail.com>
2025-02-26 10:27:35 +0100

    readme php

---
+++ README.php
@@ -0,0 +1,36 @@
+<h1 id="pictera">Pictera</h1>
+<h2 id="o-projektu">O projektu</h2>
+<p>Pictera je webová stránka pro sdílení a prohlížení obrázků a textů. Uživatelé mohou nahrávat své vlastní obrázky, prohlížet obrázky ostatních uživatelů a přidávat si obrázky do oblíbených.</p>
+<h2 id="jak-pou-vat">Jak používat</h2>
+<h3 id="registrace">Registrace</h3>
+<ol>
+<li>Otevřete registrační stránku <a href="https://app.opent2.com/it3a/knizef/Projekt1/registrace.php">Registrace</a>.</li>
+<li>Vyplňte registrační formulář a odešlete jej.</li>
+</ol>
+<h3 id="p-ihl-en-">Přihlášení</h3>
+<ol>
+<li>Otevřete přihlašovací stránku <a href="https://app.opent2.com/it3a/knizef/Projekt1/prihlaseni.php">Přihlášení</a>.</li>
+<li>Zadejte své přihlašovací údaje a přihlaste se.</li>
+</ol>
+<h3 id="nahr-v-n-obr-zk-">Nahrávání obrázků</h3>
+<ol>
+<li>Přihlaste se ke svému účtu.</li>
+<li>Klikněte na tlačítko &quot;Nahrát&quot; a vyberte levou možnost pro nahrání obrázku.</li>
+<li>Vyberte obrázek, který chcete nahrát, přidejte název, popis a místo a sdílejte.</li>
+</ol>
+<h3 id="prohl-en-obr-zk-">Prohlížení obrázků</h3>
+<ol>
+<li>Otevřete stránku postů <a href="https://app.opent2.com/it3a/knizef/Projekt1/feed.php">Feed</a>.</li>
+<li>Procházejte obrázky nahrané ostatními uživateli.</li>
+<li>Kliknutím na obrázek zobrazíte jeho detail.</li>
+<li>Kliknutím na srdíčko přidáte obrázku like a bude se vám zobrazovat na profilu</li>
+</ol>
+<h3 id="nastaven-">Nastavení</h3>
+<ol>
+<li>Otevřete stránku nastavení <a href="https://app.opent2.com/it3a/knizef/Projekt1/nastaveni.php">Nastavení</a>.</li>
+<li>Můžete si změnit: heslo, uživatelské jméno, email, profilový obrázek, režim soukromí (skrytí oblíbených postů na profilu), tmavý režim nebo si můžete smazat účet.</li>
+</ol>
+<h3 id="kontakt">Kontakt</h3>
+<p>Pokud máte jakékoliv dotazy nebo potřebujete pomoc, můžete nás kontaktovat na stránce <a href="kontakt.php">Kontakt</a>.</p>
+<h2 id="podpora">Podpora</h2>
+<p>Pokud chcete podpořit tento projekt, navštivte stránku <a href="podporit.php">Podpořit</a>.</p>
